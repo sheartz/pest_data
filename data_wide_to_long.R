1784b18a79abec01426d714b59ccb76f799a1850
Shane Heartz <57361195+sheartz@users.noreply.github.com>
2024-10-30 09:28:12 -0300

    Update data_wide_to_long.R

--- data_wide_to_long.R
+++ data_wide_to_long.R
@@ -3,6 +3,7 @@ library(sf)      # For reading shapefiles
 library(tidyr)   # For reshaping the data
 library(dplyr)   # For data manipulation
 library(stringr) # For padding sequences
+library(purrr)   # For mapping or tracking functions
 
 # Set the working directory
 wd <- "\\\\s-fre-nas1\\Data$\\cedge\\Historical_Pesticide_Project\\NS_NL_historic_pesticides\\RawData\\"
@@ -14,8 +15,8 @@ shapefile_path <- paste0(wd, "NL_NS_historic_pesticid_Project.shp")
 # Read the shapefile
 wide_data <- st_read(shapefile_path)
 
-# View the structure of the data (optional)
-str(wide_data)
+# Display column names to confirm exact names
+print(colnames(wide_data))
 
 # Create `app_area` as a sequential identifier for each `year`
 shapefile_data_unique <- wide_data %>%
@@ -25,14 +26,17 @@ shapefile_data_unique <- wide_data %>%
          app_area = paste0(year, "_", str_pad(app_area_seq, 3, pad = "0"))) %>%
   ungroup()
 
-# Create `app_id` by adding an additional sequence within each `app_area`
-shapefile_data_unique <- shapefile_data_unique %>%
-  group_by(app_area) %>%
-  mutate(app_id_seq = row_number(),
-         app_id = paste0(app_area, "_", app_id_seq)) %>%
-  ungroup() %>%
-  
-  # Select the fields to keep in the shapefile and CSV
+# Need to expand rows based on `num_apps` and create `app_id` to avoid errors use "mutate", then "unnest" to expand to separate rows
+shapefile_data_expanded <- shapefile_data_unique %>%
+  rowwise() %>%
+  mutate(
+    app_id = list(paste0(app_area, "_", seq_len(num_apps)))  # List of IDs for each app
+  ) %>%
+  unnest(app_id) %>%  # Expands each `app_area` to multiple rows based on `num_apps`
+  ungroup()
+
+# Select fields to keep in the shapefile and CSV
+shapefile_data_expanded <- shapefile_data_expanded %>%
   select(app_id, app_area, num_apps, block_1, block_2, block_3, block_4, year, province, map_nam, 
          insect_1, insect_2, insect_3, insect_4, act_ing_1, act_ing_2, act_ing_3, act_ing_4, 
          app_type, rate_1, rate_2, rate_3, rate_4, vol_Lha1, vol_Lha2, vol_Lha3, vol_Lha4, 
@@ -40,8 +44,8 @@ shapefile_data_unique <- shapefile_data_unique %>%
          form_type4, brand_1, brand_2, brand_3, brand_4, aircraft, guidance, app_sys, 
          reference, Shape_Leng, Shape_Area)
 
-# Pivot data to long format for specific attributes
-csv_data <- shapefile_data_unique %>%
+# Pivot data to long format for specific attributes for the csv write
+csv_data <- shapefile_data_expanded %>%
   pivot_longer(
     cols = starts_with(c("insect_", "act_ing_", "rate_", "vol_Lha", "form_type", "tank_mix", "brand_")),
     names_to = c(".value", "group"),
@@ -52,5 +56,5 @@ csv_data <- shapefile_data_unique %>%
 # Write the transformed data to a CSV file
 write.csv(csv_data, "attribute_data_long.csv", row.names = FALSE)
 
-# View the final long-format data (optional)
+# View the final long-format data
 print(csv_data)
